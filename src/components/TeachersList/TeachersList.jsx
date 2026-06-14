623538b9a95c56d2a46fe2ace247314782c6c3f2
Tetiana <bangchanka0310@gmail.com>
2026-06-14 23:57:49 +0300

    add button load more

--- src/components/TeachersList/TeachersList.jsx
+++ src/components/TeachersList/TeachersList.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import TeacherCard from "../TeacherCard/TeacherCard";
 import css from "./TeachersList.module.css";
 
 export default function TeachersList({ teachers }) {
+  const [visibleCount, setVisibleCount] = useState(4);
+
+  const visibleTeachers = teachers.slice(0, visibleCount);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 4);
+  };
   return (
-    <ul className={css.list}>
-      {teachers.map((teacher) => (
-        <TeacherCard key={teacher.id} teacher={teacher} />
-      ))}
-    </ul>
+    <>
+      <ul className={css.list}>
+        {visibleTeachers.map((teacher) => (
+          <TeacherCard key={teacher.id} teacher={teacher} />
+        ))}
+      </ul>
+      {visibleCount < teachers.length && (
+        <button className={css.btnLoadMore} onClick={handleLoadMore}>
+          Load More
+        </button>
+      )}
+    </>
   );
 }
